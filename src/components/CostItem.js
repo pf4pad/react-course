a2e095fc21fc159ab445a689c2bf4d529c06b5de
Pavlo Cherevach <pf4pad.fr@gmail.com>
2022-06-15 17:40:14 +0300

    react components with date

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,15 +1,15 @@
 import './CostItem.css'
 
 function CostItem() {
-  // const costDate = new Date(2022, 6, 6);
-  // const costDescription = 'Холодильник';
-  // const costAmount = 1000;
+  const costDate = new Date(2022, 6, 6);
+  const costDescription = 'Холодильник';
+  const costAmount = 1000;
   return (
     <div className="cost-item">
-      <div>vfhn</div>
+      <div>{costDate.toString()}</div>
       <div className="cost-item__description">
-        <h2>Холодильник</h2>
-        <div className="cost-item__price">asd</div>
+        <h2>{costDescription}</h2>
+        <div className="cost-item__price">$ {costAmount}</div>
       </div>
 
     </div>
